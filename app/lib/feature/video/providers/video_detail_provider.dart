592a2ad500b451f9f1cea2398ade8d2175e20345
Tyler Savery <tyler@theyoungastronauts.com>
2022-11-20 16:07:34 -0500

    removed auto generated video feature from app

--- app/lib/feature/video/providers/video_detail_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:houston_app/feature/video/models/video.dart';
-import 'package:houston_app/feature/video/services/video_service.dart';
-
-final videoDetailProvider =
-    FutureProvider.family<Video, String>((ref, String uuid) async {
-  return VideoService().retrieve(uuid);
-});
