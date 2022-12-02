04c4a558cf2826a74563777357c9787f8ea79eb1
Tyler Savery <tyler@theyoungastronauts.com>
2022-12-02 14:27:26 -0500

    user following

--- service/api/user/views.py
+++ service/api/user/views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth import get_user_model
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
+from rest_framework import status
+from rest_framework.response import Response
 
+from connect.models import Follow
 from api.permissions import IsAuthenticated
 from api.user.filters import UserFilter
 from api.user.querysets import ALL_USERS_QUERYSET
-from api.user.serializers import UserPublicSerializer, UserSerializer
+from api.user.serializers import (
+    UserPublicSerializer,
+    UserSerializer,
+    UserFollowSerializer,
+)
 
 User = get_user_model()
 
@@ -40,3 +47,29 @@ class UserMeRetrieveUpdateView(RetrieveModelMixin, UpdateModelMixin, UserAPIView
 
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
+
+
+class UserFollowView(GenericAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserFollowSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = self.get_object()
+        owner = serializer.validated_data.get("owner")
+
+        if bool(kwargs.get("will_follow")):
+            Follow.objects.get_or_create(user=user, owner=owner)
+        else:
+            try:
+                follow = Follow.objects.get(user=user, owner=owner)
+            except Follow.DoesNotExist:
+                pass
+            else:
+                follow.delete()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
---
+++ service/connect/models/follow.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from project.models import AbstractModel
+from django.utils.translation import gettext_lazy as _
+
+
+User = get_user_model()
+
+
+class Follow(AbstractModel):
+
+    user = models.ForeignKey(
+        User,
+        verbose_name=_("user"),
+        related_name="follow_users",
+        on_delete=models.CASCADE,
+    )
+    owner = models.ForeignKey(
+        User,
+        verbose_name=_("owner"),
+        related_name="follow_owners",
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = _("user like")
+        verbose_name_plural = _("user likes")
+        unique_together = [("user", "owner")]
