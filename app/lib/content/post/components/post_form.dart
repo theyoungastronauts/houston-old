34b9db63f10c2ea81226c2f1a9e2709702b9a908
Manuel Chalá <manuel@theyoungastronauts.com>
2022-12-02 12:28:30 -0500

    Strip the markdown editor in the post form

--- app/lib/content/post/components/post_form.dart
+++ app/lib/content/post/components/post_form.dart
@@ -7,7 +7,6 @@ import 'package:simple_markdown_editor/widgets/markdown_toolbar.dart';
 import '../../../core/components/base_component.dart';
 import '../../../media/asset/components/multi_asset_manager.dart';
 import '../providers/post_form_provider.dart';
-import 'markdown_toolbar_icon.dart';
 
 class PostForm extends BaseComponent {
   const PostForm({Key? key}) : super(key: key);
@@ -31,60 +30,12 @@ class PostForm extends BaseComponent {
                 validator: provider.titleValidator,
                 decoration: const InputDecoration(label: Text("Title")),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Body',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    SizedBox(
-                      height: 100,
-                      width: 1500,
-                      child: MarkdownFormField(
-                        focusNode: _focusNode,
-                        controller: provider.bodyController,
-                        emojiConvert: true,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        MarkDownToolbarIcon(
-                          controller: provider.bodyController,
-                          icon: FontAwesomeIcons.bold,
-                          leading: '**',
-                          trailing: '**',
-                        ),
-                        MarkDownToolbarIcon(
-                          controller: provider.bodyController,
-                          icon: FontAwesomeIcons.italic,
-                          leading: '*',
-                          trailing: '*',
-                        ),
-                        MarkDownToolbarIcon(
-                          controller: provider.bodyController,
-                          icon: FontAwesomeIcons.heading,
-                          leading: '# ',
-                          trailing: '',
-                        ),
-                        MarkDownToolbarIcon(
-                          controller: provider.bodyController,
-                          icon: FontAwesomeIcons.code,
-                          leading: '`',
-                          trailing: '`',
-                        ),
-                        MarkDownToolbarIcon(
-                          controller: provider.bodyController,
-                          icon: FontAwesomeIcons.strikethrough,
-                          leading: '~~',
-                          trailing: '~~',
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+              TextFormField(
+                controller: provider.bodyController,
+                validator: provider.bodyValidator,
+                decoration: const InputDecoration(label: Text("Body")),
+                minLines: 3,
+                maxLines: 3,
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8),
